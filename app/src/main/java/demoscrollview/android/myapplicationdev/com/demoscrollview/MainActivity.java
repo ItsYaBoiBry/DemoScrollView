91757312d253696ddb330dc15bc42cc287837e77
ItsYaBoiBry <bryanlow987@gmail.com>
2017-04-20 12:59:41 +0800

    added buttons which adds whatever text the Edit text box inputs to a new textView

--- app/src/main/java/demoscrollview/android/myapplicationdev/com/demoscrollview/MainActivity.java
+++ app/src/main/java/demoscrollview/android/myapplicationdev/com/demoscrollview/MainActivity.java
@@ -2,12 +2,36 @@ package demoscrollview.android.myapplicationdev.com.demoscrollview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    EditText et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final LinearLayout mlayout = (LinearLayout)findViewById(R.id.linearLayout);
+        btn = (Button)findViewById(R.id.button);
+        et = (EditText)findViewById(R.id.editText);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mlayout.addView(createNewTextView(et.getText().toString()));
+            }
+        });
+
+    }
+    private TextView createNewTextView(String text) {
+        final LinearLayoutCompat.LayoutParams lparams = new LinearLayoutCompat.LayoutParams(LinearLayoutCompat.LayoutParams.WRAP_CONTENT, LinearLayoutCompat.LayoutParams.WRAP_CONTENT);
+        final TextView textView = new TextView(this);
+        textView.setLayoutParams(lparams);
+        textView.setText(text);
+        return textView;
     }
 }
